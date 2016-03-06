c810ab35261acb3b007bfca2383250ecb8d6890e
kele <damian.bogel@gmail.com>
2016-03-06 19:07:17 +0100

    stuff I install on top of Xubuntu

---
+++ stuff_to_install.sh
@@ -0,0 +1,39 @@
+sudo apt-get install \
+    arandr          `# multi-monitor setup made easy` \
+    clang           `# C compiler` \
+    cmake           `# build system` \
+    dolphin         `# light file manager` \
+    feh             `# bakground setting` \
+    fonts-inconsolata `# my fav font for terminal` \
+    gimp            `# image editing` \
+    git             `# VCS` \
+    gparted         `# disk management tool` \
+    gtk3-engines-oxygen `# nice gtk3 theme` \
+    gtk-theme-switch `# GTK settings (for i3)` \
+    htop            `# better top` \
+    i3lock          `# lockscreen` \
+    i3              `# the best window manager ever` \
+    lxappearance    `# GTK settings once again (for i3)` \
+    meld            `# merge utility` \
+    newsbeuter      `# rss reader` \
+    okular          `# pdf viewer` \
+    qt4-qtconfig    `# Qt settings (for i3)` \
+    qtcreator       `# C++ IDE` \
+    racket          `# Scheme dialect with nice IDE` \
+    redshift-gtk    `# screen dimming for reduced eye strain` \
+    ristretto       `# image viewer` \
+    rlwrap          `# needed for some console apps to behave sane` \
+    scrot           `# screenshot utility` \
+    tmux            `# terminal manager` \
+    transmission-gtk    `# torren client` \
+    trash-cli       `# command line trash` \
+    unar            `# archive utility` \
+    valgrind        `# memory leak analysis and more` \
+    vim             `# <3` \
+    vlc             `# movies` \
+    zathura         `# simple pdf viewer` \
+    zsh             `# shell`
+
+
+
+
