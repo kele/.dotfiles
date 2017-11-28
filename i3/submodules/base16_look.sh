1a12c4ba67858f76280fbd39ef4ad727739d458b
kele <damian.bogel@gmail.com>
2017-11-28 22:05:25 +0100

    base16 fixes

--- i3/submodules/base16_look.sh
+++ i3/submodules/base16_look.sh
@@ -1,27 +1,20 @@
 if [ -n "$BASE16_LOOK_INCLUDED" ]
 then
-    BASE16_LOOK_INCLUDED="yes"
+    exit 0
 else
 
+BASE16_LOOK_INCLUDED="yes"
+
+source ./base16_palette.sh
+
 cat << EndOfConfig
-# Colors taken from github.com/tomfulghum/dotfiles
-# Heavily inspired by base16-tomorrow
-set \$base00 #1d1f21
-set \$base01 #282a2e
-set \$base02 #373b41
-set \$base03 #969896
-set \$base04 #b4b7b4
-set \$base05 #c5c8c6
-set \$base06 #e0e0e0
-set \$base07 #ffffff
-set \$base08 #cc6666
-set \$base09 #de935f
-set \$base0A #f0c674
-set \$base0B #b5bd68
-set \$base0C #8abeb7
-set \$base0D #81a2be
-set \$base0E #b294bb
-set \$base0F #a3685a
+# Window colors
+#                       border      background  text        indicator
+client.focused          \$base0D     \$base0D     \$base00     \$base01
+client.focused_inactive \$base02     \$base02     \$base03     \$base01
+client.unfocused        \$base01     \$base01     \$base03     \$base01
+client.urgent           \$base02     \$base08     \$base07     \$base08
+
 EndOfConfig
 
 fi
