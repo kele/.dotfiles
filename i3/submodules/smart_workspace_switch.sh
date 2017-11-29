a737f382cc748272957fa2b015729557f024c194
kele <damian.bogel@gmail.com>
2017-11-29 20:05:56 +0100

    all  fixes

--- i3/submodules/smart_workspace_switch.sh
+++ i3/submodules/smart_workspace_switch.sh
@@ -6,8 +6,8 @@ SMART_WORKSPACE_SWITCH_INCLUDED="yes"
 cat << EndOfConfig
 # INCLUDE: submodules/smart_workspace_switch.sh
 
-bindsym \$mod+Tab exec \$WORKING_DIR/next_workspace_ignore_10.py
-bindsym \$mod+Shift+Tab exec \$WORKING_DIR/prev_workspace_ignore_10.py
+bindsym \$mod+Tab exec $WORKING_PATH/next_workspace_ignore_10.py
+bindsym \$mod+Shift+Tab exec $WORKING_PATH/prev_workspace_ignore_10.py
 
 EndOfConfig
 
